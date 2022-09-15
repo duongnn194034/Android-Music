05f3647d209bfee1c6d9c45630156443fdb9bb4f
DuongNN194034 <laserbean4@gmail.com>
2022-09-15 21:41:22 +0700

    first commit

---
+++ project/app/build/generated/source/buildConfig/androidTest/debug/com/iven/musicplayergo/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.iven.musicplayergo.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.iven.musicplayergo.test";
+  public static final String BUILD_TYPE = "debug";
+}
